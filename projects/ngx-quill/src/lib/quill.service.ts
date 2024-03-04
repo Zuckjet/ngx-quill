16147659dc6d2bc8588fdd538dd58e0228266248
Ligayu <60767980+Ligayu@users.noreply.github.com>
2024-03-04 09:41:43 +0800

    fix: 调整引入的包

--- projects/ngx-quill/src/lib/quill.service.ts
+++ projects/ngx-quill/src/lib/quill.service.ts
@@ -37,7 +37,7 @@ export class QuillService {
         // eslint-disable-next-line @typescript-eslint/dot-notation
         this.document['__zone_symbol__addEventListener'] ||
         this.document.addEventListener
-      const quillImport = await import('quill')
+      const quillImport = await import('quill-zuckjet')
       this.document.addEventListener = maybePatchedAddEventListener
 
       this.Quill = (
