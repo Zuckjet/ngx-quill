c25050bd1baec41450cc5795f2bc3bbc7c3883ca
Ligayu <60767980+Ligayu@users.noreply.github.com>
2025-02-11 10:43:14 +0800

    chore: chromium no sandbox

--- projects/ngx-quill/karma.conf.js
+++ projects/ngx-quill/karma.conf.js
@@ -41,6 +41,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
+    flags: ['--no-sandbox'],
     singleRun: false,
     restartOnFileChange: true
   });
